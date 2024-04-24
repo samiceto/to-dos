499978d66dbbf44cd1a496506ec844757b3fb123
samiceto <you@example.com>
2024-04-25 00:37:31 +0500

    ready to use project todos

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/cnv node
+
+import inquirer from "inquirer"
+
+let todos=[]
+let condition=true;
+
+while(condition){
+
+let todoslist=await inquirer.prompt([
+    {
+        name:"firstname",
+        type: "input",
+        message: "would you like to add some list in your todos?"
+
+},
+{
+    name:"secondname",
+    type:"confirm",
+    message:"would you like to add some more?",
+    default:true
+}
+])
+todos.push(todoslist.firstname);
+console.log(todos);
+condition=todoslist.secondname;
+}
